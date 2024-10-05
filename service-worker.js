d520ae6bf2fe2b30f296b3bad17ad157468e7536
Gabriel Salazar <133398841+GaboscaNullcode@users.noreply.github.com>
2024-10-05 17:06:32 -0500

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'mi-pwa-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/app.js',
+  '/cdn-cgi/imagedelivery/ZIty0Vhmkm0nD-fBKJrTZQ/test_prueba:NullCode.png/128',
+  '/cdn-cgi/imagedelivery/ZIty0Vhmkm0nD-fBKJrTZQ/test_prueba:NullCode.png/540'
+];
+
+// Instalación del Service Worker y almacenamiento en caché
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Interceptar solicitudes de red para servir desde la caché
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response; // Devuelve desde la caché
+        }
+        return fetch(event.request); // Si no está en la caché, realiza la solicitud a la red
+      })
+  );
+});
